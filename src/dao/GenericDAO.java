914180abfdebc815861b61bd6cdc1ca9b2788b5e
valentina <valenpascutti2612@gmail.com>
2025-11-15 10:55:33 -0300

    Agrego métodos con Connection al GenericDAO

--- src/dao/GenericDAO.java
+++ src/dao/GenericDAO.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface GenericDAO<T> {
@@ -8,5 +10,12 @@ public interface GenericDAO<T> {
     void eliminar(Long id);
     T obtenerPorId(Long id);
     List<T> listarTodos();
+    void insertar(T entidad, Connection conn) throws SQLException;
+    void actualizar(T entidad, Connection conn) throws SQLException;
+    void eliminar(Long id, Connection conn) throws SQLException;
+    T obtenerPorId(Long id, Connection conn) throws SQLException;
+    List<T> listarTodos(Connection conn) throws SQLException;
+
 }
+
 
